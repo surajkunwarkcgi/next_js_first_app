cb949ecd4a08aca5c3f689fb6821372db7109e28
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2024-08-31 19:08:01 +0900

    Project Setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@styles/globals.css";
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +8,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
     </html>
   );
 }
---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span>AI-Powered Prompts</span>
+      </h1>
+    </section>
+  );
+};
+
+export default Home;
