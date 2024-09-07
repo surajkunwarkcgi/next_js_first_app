c598ec6ce877fca986d6500f9ab8887c9f827eff
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2024-09-07 18:45:44 +0900

    google authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -9,11 +11,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">{children}</main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
