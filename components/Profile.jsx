d94a57fab4cf7da6f002bb79b0ac7c320861c3af
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2024-09-15 19:15:20 +0900

    edit post

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import PromptCard from "./PromptCard";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">{name} Profile</h1>
+      <p className="desc text-left">{desc}</p>
+      <div className="mt-16 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
